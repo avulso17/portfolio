e0262a7444f89714451007a04e799526a5d3c5b3
Felipe Mateus <felipe_mateus08@hotmail.com>
2023-07-18 18:02:53 -0300

    feat(tailwind): switch some css props in global to apply tailwind rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   content: ['./src/**/*.{jsx,tsx,mdx}'],
   theme: {
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+      roboto: ['Roboto', 'sans-serif'],
+    },
+    screens: {
+      xs: '480px',
+      sm: '620px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    opacity: {
+      0: '0',
+      15: '0.15',
+      25: '0.25',
+      50: '0.5',
+      80: '0.8',
+      100: '1',
+    },
     extend: {
       colors: {
         primary: '#1a202c',
@@ -10,6 +29,9 @@ module.exports = {
         // aliases
         text: '#f3f3f3',
         background: '#222222',
+      },
+      borderRadius: {
+        round: '50%',
       },
     },
   },
